a23301c3fdf0b71ae4d32c54c9fb514d5e9cabee
Jason Vu <Jasonvu77@gmail.com>
2025-08-20 00:52:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function generateQRCode() {  
 document.getElementById("spinner").style.display = "block";
 document.getElementById("qrcode").innerHTML = "";
+document.getElementById("copyLink").addEventListener("click", () => {
+  const input = document.getElementById("qrInput");
+  const copyPopup = document.getElementById("copyPopup")
+  if (input.value) {
+    navigator.clipboard.writeText(input.value).then(() => {
+        copyPopup.classList.add("show");
+        setTimeout(() => {
+            copyPopup.classList.remove("show");
+        }, 2000);
+    });
+  }
+});
 const url = document.getElementById("qrInput").value
 if (url.trim() === "") {
     document.getElementById("warning").textContent = "Please enter a valid URL.";
@@ -20,3 +32,4 @@ console.log("generated QR for: " + url);
 }, 2000);
 }
 document.getElementById("qrBtn").addEventListener("click", generateQRCode);
+
